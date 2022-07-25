e37305d6841fddec3255f844ca6ea4e71ff10562
Amrabdelmoneim <amrahmed1200@gmail.com>
2022-07-25 21:10:25 +0200

    my code

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+
+	char c;
+	printf("enter the character you want :\n");
+	scanf("%c",&c);
+    printf("ASCI value for %c = %%d",c,c);
+
+
+
+	return 0;
+
+}
